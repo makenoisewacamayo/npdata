fa12b6071ce66c2e7c49389d944c3e021754f2c1
k3n3l <ahenriquez@gmail.com>
2014-01-28 20:26:56 -0300

    PigLatin comparator code generator was included (II)

--- src/generator/__init__.py
+++ src/generator/__init__.py
@@ -1,7 +1,22 @@
 from abc import ABCMeta, abstractmethod
+from avro.schema import PrimitiveSchema
 
 class AbstractCodeGenerator:
     __metaclass__ = ABCMeta
+
+    equivalence = {}
+
+    def get_equivalence(self, field, value):
+        if not type(field.type) is PrimitiveSchema:
+            raise Exception('Only primitive schemas are implemented')
+
+        if not field.type.type in self.equivalence:
+            raise Exception('Map for {0} was not found' % field.type.type)
+
+        if not value in self.equivalence[field.type.type]:
+            raise Exception('{0} equivalence was not found' % value)
+
+        return self.equivalence[field.type.type][value]
 
     @abstractmethod
     def make_file_name(self, schema):
